890baa4f6a0674d9a92415f3123e9fee38876f94
mattthughes <matt.h17@yahoo.com>
2026-01-23 23:49:09 +0000

    Added tailwind css to project

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.css"
+
+const App = () => {
+  return (
+    <div>
+      <section className='h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl'>
+        <h1>Hello World</h1>
+      </section>
+    </div>
+  )
+}
+
+export default App
