b163a0823cf81615416b06f2df4fc49ad974ee3e
mattthughes <matt.h17@yahoo.com>
2026-01-24 12:50:29 +0000

    Added basic navbar to set up routing

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import "./index.css"
+import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
-    <div>
-      <section className='h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl'>
-        <h1>Hello World</h1>
-      </section>
-    </div>
+    <>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </>
+    
   )
 }
 
