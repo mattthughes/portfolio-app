94606bcaded3e58b22db12e6a2235472bb9cf241
mattthughes <matt.h17@yahoo.com>
2026-01-24 16:57:07 +0000

    Added basic template to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 import '../../index.css'
 
 const Home = () => {
-  return (
-    <div className="bg-green-400">
-      <h1>Hello world</h1>
-    </div>
-  )
+    return (
+        <>
+            <section>
+                <div className="flex justify-center">
+                    <img className="w-44 h-44 lg:w-44 lg:h-44 rounded-md" src="/docs/images/portfolio-image.jpg" />
+                </div>
+                <div className="flex">
+                    <div className="flex-1 text-center">
+                        <h1 className="">Junior software developer</h1>
+                        <p className="">Hi my name is Matthew Hughes, I am a passionate front-end React developer based in Southend-On-Sea England. </p>
+                    </div>
+                </div>
+                
+
+            </section>
+
+        </>
+
+    )
 }
 
 export default Home
+
