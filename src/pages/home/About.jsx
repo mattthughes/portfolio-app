510669860a4d345097134765efaaf1bb5c901e32
Matthew Hughes <matt.h17@yahoo.com>
2026-06-06 11:39:34 +0100

    Added footer and about section styling

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -3,13 +3,15 @@ import "../../index.css";
 
 const About = () => {
     return (
-        <div id="about" className= "flex flex-col items-center gap-10 lg:gap-3">
+        <section id="about" className= "pt-12 pb-16">
+        <div id="about" className= "max-w-7xl mx-auto px-4">
             <h3 className="text-3xl font-bold">About Me</h3>
-            <p className="text-lg text-center p-1">
+            <p className="text-lg p-1 mt-4">
                 I am a Fullstack software developer with experience in creating modern web applications, such as Souls like Gallery and Last trophy, I enjoy building and creating applications which add value to users, 
                 such as making an entitlement calculator which allows the user to easily calculate there remaining holiday.
             </p>
         </div>
+        </section>
     )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -94,6 +94,13 @@ const Home = () => {
             </section>
             <About/>
             <Projects />
+            <footer className="bg-gray-800 text-white py-6 mt-12">
+                <div className="max-w-7xl mx-auto px-4">
+                    <p className="text-center">
+                        &copy; {new Date().getFullYear()} Matthew Hughes. All rights reserved.
+                    </p>
+                </div>
+            </footer>
         </>
     );
 };
